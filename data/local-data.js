ecc5a02d91650a9df618ed44e10da3b23696fbc3
MichLiu <602502634@qq.com>
2018-12-14 10:01:07 +0800

    50%

---
+++ data/local-data.js
@@ -0,0 +1,36 @@
+var local_edu_database = [
+    {
+        infoId:1,
+        title: "2018年下半年全国",
+        infoTime: "2018/12/2",
+        randomColor: "#ff0000",
+    },
+    {
+        infoId:2,
+        title: "2018-2019学年第一",
+        infoTime: "2018/11/1",
+        randomColor: "#eb4310"
+    },
+    {
+        infoId:3,
+        title: "2018年下半年全国",
+        infoTime: "2019/2/1",
+        randomColor: "#f6941d"
+    },
+    {
+        infoId: 4,
+        title: "这是个demo",
+        infoTime: "2019/12/1",
+        randomColor: "#fbb417"
+    },
+    {
+        infoId: 5,
+        title: "这又是一个demo",
+        infoTime: "2018/11/1",
+        randomColor: "#a1488e"
+    }
+]
+
+module.exports = {
+    infoList: local_edu_database
+}
--- pages/infos/info.js
+++ pages/infos/info.js
@@ -1,16 +1,50 @@
+var infosData = require('../../data/local-data.js')
 Page({
 
   /**
    * 页面的初始数据
    */
   data: {
-    
+    showView: true,
+    rotateData: null
   },
 
+  //当用户点击按钮，逻辑层将showView切换为其相反布尔类型并传给视图层。用let定义变量，防止全局污染。
+  showButton:function () {
+    let that = this;
+    //根据按钮的行为对应导航栏的背景颜色
+    if(that.data.showView == true){
+      wx.setNavigationBarColor({
+        frontColor: '#ffffff',
+        backgroundColor: '#7be85a'
+      })
+    }else{
+      wx.setNavigationBarColor({
+        frontColor: '#ffffff',
+        backgroundColor: '#5A9DE8'
+      })
+    }
+
+    //点击图片切换showView的相反布尔型传给视图层
+    that.setData({
+      showView: (!that.data.showView)
+    })
+  },
+
+  onInfoTap: function (event) {
+
+    var infoId = event.currentTarget.dataset.infoid;
+    wx.navigateTo ({
+      url: "info-detail/info-detail?id=" +infoId
+    })
+  },
   /**
    * 生命周期函数--监听页面加载
    */
   onLoad: function (options) {
+    this.setData({
+      infoList: infosData.infoList
+    });
     
   },
 
@@ -18,7 +52,11 @@ Page({
    * 生命周期函数--监听页面初次渲染完成
    */
   onReady: function () {
-    
+    var animation = wx.createAnimation({})
+    animation.rotate(360).step({duration:3000})
+    this.setData({
+      rotateData: animation.export()
+    })
   },
 
   /**
@@ -41,25 +79,4 @@ Page({
   onUnload: function () {
     
   },
-
-  /**
-   * 页面相关事件处理函数--监听用户下拉动作
-   */
-  onPullDownRefresh: function () {
-    
-  },
-
-  /**
-   * 页面上拉触底事件的处理函数
-   */
-  onReachBottom: function () {
-    
-  },
-
-  /**
-   * 用户点击右上角分享
-   */
-  onShareAppMessage: function () {
-    
-  }
 })
